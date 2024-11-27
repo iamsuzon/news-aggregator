18a6a00e9b175e7e381be01502c81905281ceee6
DESKTOP-6QFRUIJ\SARK <misujon01@gmail.com>
2024-11-27 18:05:21 +0600

    seeder modified for automate data fetching

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Artisan::call('scrap:article');
+        Artisan::call('queue:work', [
+            '--timeout' => 60,
+            '--tries' => 1,
+            '--delay' => 1,
+            '--stop-when-empty' => true
         ]);
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('scrap:article')->daily();
+
+Schedule::call(function () {
+    Artisan::call('queue:work', [
+        '----stop-when-empty' => true,
+        '--timeout' => 60,
+        '--tries' => 1,
+    ]);
+})->everyMinute();
